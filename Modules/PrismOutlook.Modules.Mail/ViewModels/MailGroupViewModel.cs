9ebbc341e2f16f91e6cb5796c7c9d55d55825b42
Florin CIOBANU <29834996+florin141@users.noreply.github.com>
2020-11-21 01:06:31 +0100

    More navigation plumbing

--- Modules/PrismOutlook.Modules.Mail/ViewModels/MailGroupViewModel.cs
+++ Modules/PrismOutlook.Modules.Mail/ViewModels/MailGroupViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.ObjectModel;
+using Prism.Commands;
 using Prism.Mvvm;
 using PrismOutlook.Business;
+using PrismOutlook.Core;
 
 namespace PrismOutlook.Modules.Mail.ViewModels
 {
     public class MailGroupViewModel : BindableBase
     {
+        private readonly IApplicationCommands _applicationCommands;
         private ObservableCollection<NavigationItem> _items;
         public ObservableCollection<NavigationItem> Items
         {
@@ -13,9 +16,22 @@ namespace PrismOutlook.Modules.Mail.ViewModels
             set { SetProperty(ref _items, value); }
         }
 
-        public MailGroupViewModel()
+        private DelegateCommand<NavigationItem> _selectedCommand;
+        public DelegateCommand<NavigationItem> SelectedCommand =>
+            _selectedCommand ?? (_selectedCommand = new DelegateCommand<NavigationItem>(ExecuteSelectedCommand));
+
+        void ExecuteSelectedCommand(NavigationItem item)
+        {
+            if (item != null)
+            {
+                _applicationCommands.NavigateCommand.Execute(item.NavigationPath);
+            }
+        }
+
+        public MailGroupViewModel(IApplicationCommands applicationCommands)
         {
             GenerateMenu();
+            _applicationCommands = applicationCommands;
         }
 
         void GenerateMenu()
@@ -24,23 +40,23 @@ namespace PrismOutlook.Modules.Mail.ViewModels
 
             var root = new NavigationItem
             {
-                Caption = "Personal Folder", 
-                NavigationPath = "MailList"
+                Caption = "Personal Folder",
+                NavigationPath = "MailList?id=Default"
             };
             root.Items.Add(new NavigationItem
             {
-                Caption = "Inbox", 
-                NavigationPath = ""
+                Caption = "Inbox",
+                NavigationPath = "MailList?id=Inbox"
             });
             root.Items.Add(new NavigationItem
             {
-                Caption = "Deleted", 
-                NavigationPath = ""
+                Caption = "Deleted",
+                NavigationPath = "MailList?id=Deleted"
             });
             root.Items.Add(new NavigationItem
             {
-                Caption = "Sent", 
-                NavigationPath = ""
+                Caption = "Sent",
+                NavigationPath = "MailList?id=Sent"
             });
 
             Items.Add(root);
--- Modules/PrismOutlook.Modules.Mail/ViewModels/MailListViewModel.cs
+++ Modules/PrismOutlook.Modules.Mail/ViewModels/MailListViewModel.cs
@@ -3,14 +3,36 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Prism.Regions;
 
 namespace PrismOutlook.Modules.Mail.ViewModels
 {
-    public class MailListViewModel : BindableBase
+    public class MailListViewModel : BindableBase, INavigationAware
     {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
         public MailListViewModel()
         {
+            Title = "Default";
+        }
 
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            Title = navigationContext.Parameters.GetValue<string>("id");
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
         }
     }
 }
